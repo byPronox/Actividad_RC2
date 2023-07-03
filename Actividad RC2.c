9f3c503be00308ee26e8010262f0c1eb02ccfa7c
Laboratorio <laboratorio@udla-ec.int>
2023-07-03 15:54:07 -0500

    La definicion de las variables dentro de int main junto con su arreglo tridimencional

--- Actividad RC2.c
+++ Actividad RC2.c
@@ -4,88 +4,77 @@
 
 #include <time.h>
 
-
-
-
 char *materias[] = {
 
-  "Ingenieria de Software",
+    "Ingenieria de Software",
 
-  "Administracion",
+    "Administracion",
 
-  "Economia",
+    "Economia",
 
-  "Relaciones Internacionales",
+    "Relaciones Internacionales",
 
-  "Matematicas",
+    "Matematicas",
 
-  "Contabilidad",
+    "Contabilidad",
 
-  "Ingenieria Industrial"};
+    "Ingenieria Industrial"};
 
+void CAMI(int Datos[][2][7], FILE *archivoResultados)
+{
 
+    int Ami = 0; // Año de máximo ingreso
 
+    int Mi = 0; // Número máximo de ingreso
 
-void CAMI (int Datos[][2][7], FILE *archivoResultados) {
+    for (int a = 0; a < 5; a++)
+    {
 
-  int Ami = 0; // Año de máximo ingreso
+        int ti = 0; // Total de ingresos se inicializa en 0
 
-  int Mi = 0;  // Número máximo de ingreso
+        for (int s = 0; s < 2; s++)
+        {
 
+            for (int c = 0; c < 7; c++)
+            {
 
+                ti += Datos[a][s][c];
+            }
+        }
 
+        if (ti > Mi)
+        {
 
-  for (int a = 0; a < 5; a++) {
+            Mi = ti;
 
-    int ti = 0; // Total de ingresos se inicializa en 0
-
-
-
-
-    for (int s = 0; s < 2; s++) {
-
-      for (int c = 0; c < 7; c++) {
-
-        ti += Datos[a][s][c];
-
-      }
-
+            Ami = 2023 - 5 + a;
+        }
     }
 
-
-
-
-    if (ti > Mi) {
-
-      Mi = ti;
-
-      Ami = 2023 - 5 + a;
-
-    }
-
-  }
-
-
-
-
-  fprintf(archivoResultados, "El año con la mayor cantidad de alumnos ingresaron a la universidad fue: %d\n", Ami);
-
+    fprintf(archivoResultados, "El año con la mayor cantidad de alumnos ingresaron a la universidad fue: %d\n", Ami);
 }
 
-void mostrarMenu() {
+void mostrarMenu()
+{
 
-  printf("Menu:\n");
+    printf("Menu:\n");
 
-  printf("1. Calcular el año con la mayor cantidad de alumnos ingresados a la universidad.\n");
+    printf("1. Calcular el año con la mayor cantidad de alumnos ingresados a la universidad.\n");
 
-  printf("2. Calcular la carrera que recibió la mayor cantidad de alumnos en el último año.\n");
+    printf("2. Calcular la carrera que recibió la mayor cantidad de alumnos en el último año.\n");
 
-  printf("3. Calcular el año en que la carrera de Ingeniería de Software recibió la mayor cantidad de alumnos.\n");
+    printf("3. Calcular el año en que la carrera de Ingeniería de Software recibió la mayor cantidad de alumnos.\n");
 
-  printf("4. Salir.\n");
-
+    printf("4. Salir.\n");
 }
 
+int main()
+{
+    srand(time(NULL));
 
+    int Datos[5][2][7];
 
+    int a, s, c;
 
+    return 0;
+}
